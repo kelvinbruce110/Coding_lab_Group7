ff6dd80036c291ffa59b76d144e198d782b5188b
airumva2-sudo <a.irumva2@alustudent.com>
2026-06-03 04:53:22 -0700

    my commit

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -7,7 +7,28 @@ process_vitals() {
 
 #member 6
 water_audit() {
+  awk -F'|' '
 
+    /ICU_WATER_RESERVE/ {
+        sum += $3
+        count++
+    }
+
+    END {
+        if (count > 0) {
+            avg = sum / count
+
+            printf "\n===== WATER AUDIT REPORT =====\n"
+            printf "Average ICU Water Usage: %.2f\n", avg
+            printf "Records Processed: %d\n", count
+            printf "==============================\n"
+        }
+        else {
+            printf "\nNo ICU_WATER_RESERVE records found.\n"
+        }
+    }
+
+    ' active_logs/water_usage.log
 }
 
 
