c5e9fd8e6d793e186d93633e7b2e50a6cca922dd
kelvinbruce110 <b.mupenzi1@alustudent.com>
2026-06-03 04:58:57 -0700

    Clinical Analyst

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -2,7 +2,16 @@
 
 #member 5
 process_vitals() {
+echo "Generating critical alerts report..."
 
+    grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log | \
+    awk -F'|' '
+    {
+        printf "Timestamp: %s | Device_ID: %s | Value: %s\n",
+        $1, $2, $3
+    }' > reports/critical_alerts.txt
+
+    echo "Report saved to reports/critical_alerts.txt"
 }
 
 #member 6
