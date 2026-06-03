755c453ca34c4d0f253d968445c3bee7f420ec22
kelvinbruce110 <b.mupenzi1@alustudent.com>
2026-06-03 01:53:46 -0700

    Initial project setup

---
+++ hospital_admin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#member 1
+initialize_system() {
+
+}
+
+#member 2
+secure_data() {
+
+}
+
+#member 3
+
---
+++ hospital_analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#member 5
+process_vitals() {
+
+}
+
+#member 6
+water_audit() {
+
+}
+
+
