29e56ad27c256b43b1c00563295c2f355c80e3ab
Tsungirirai Machingura <t.machingur@alustudent.com>
2026-06-03 03:25:53 -0700

    Archive logs using timestamps

--- hospital_archive.sh
+++ hospital_archive.sh
@@ -10,4 +10,19 @@ archive_log() {
     local logfile
     local filename
     local extension
+    
+    logfile="$1"
+
+    if [ -f "$logfile" ]; then
+
+        filename="$(basename "$logfile")"
+        extension="${filename%.log}"
+
+        mv "$logfile" \
+            "archived_logs/${extension}_${timestamp}.log"
+
+        touch "$logfile"
+
+        echo "Archived: ${extension}.log"
+    fi
 }
